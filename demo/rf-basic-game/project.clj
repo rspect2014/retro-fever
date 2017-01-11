03114afa42986984dbe720306ba4ff93d5c87638
Claus Engel-Christensen <cec@casalogic.dk>
2017-01-11 13:51:55 +0100

    Update demo dependencies

--- demo/rf-basic-game/project.clj
+++ demo/rf-basic-game/project.clj
@@ -2,10 +2,10 @@
   :description "Demoing a basic game using Retro Fever"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
                  [retro-fever "0.2.1-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
   :cljsbuild {
     :builds [{
         :source-paths ["src"]
--- demo/rf-input1/project.clj
+++ demo/rf-input1/project.clj
@@ -1,10 +1,10 @@
 (defproject rf-input1 "0.1.0-SNAPSHOT"
   :description "Demo of the input module for Retro Fever"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
                  [retro-fever "0.2.1-SNAPSHOT"]
                  [enfocus "2.1.0"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
   :cljsbuild {
     :builds [{
         :source-paths ["src"]
